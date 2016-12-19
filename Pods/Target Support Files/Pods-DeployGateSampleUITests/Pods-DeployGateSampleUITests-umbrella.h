5ef3c0b3466b148a7cd29490230fee1ccc078925
ogusu <xogusu.takuya@contract.isid.co.jp>
2016-12-19 10:49:07 +0900

    test

--- Pods/Target Support Files/Pods-DeployGateSampleUITests/Pods-DeployGateSampleUITests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_DeployGateSampleUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_DeployGateSampleUITestsVersionString[];
-
